0e8519baefe60316617bd306edfacffed5c2b955
Luciano F. Gonçalves <luciano.fg.dv@gmail.com>
2022-03-27 14:43:16 -0300

    Adicionando react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import './App.css';
-import SignIn from "./pages/signin";
-import Reading from "./pages/reading";
-import imagem from './resources/temp_image_reading.png'
-import {CancelButton, OkayButton, SignButton} from "./components/buttons";
+import {Link} from "react-router-dom";
 
 function App() {
-    const article = {
-        title: "What was the trend in 2020 and you didn’t use it",
-        text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.\nLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.\nLorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.".split("\n"),
-        author: {
-            name: "An Author",
-            work: "A work",
-            img: imagem,
-        },
-        tags: ["tag1", "tag2"],
-        temps: 4,
-    }
-
     return (
-    <div className="App">
-      <h1>Articles</h1>
-      <OkayButton text="Testando" onClick={() => alert("Alerta")}/>
-        <p><CancelButton text="Testando" onClick={() => alert("Alerta")}/></p>
-        <p><SignButton text="Testando" onClick={() => alert("Alerta")}/></p>
-    </div>
-  );
+        <div>
+            <h1>Homepage</h1>
+            <p><Link to={"/sign-in"}>"Sign In"</Link></p>
+            <p><Link to={"/reading"}>"Reading"</Link></p>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import SignIn from "./pages/signin";
+import Reading from "./pages/reading";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<App />} />
+                <Route path='/sign-in' element={<SignIn />}/>
+                <Route path='/reading' element={<Reading />}/>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
